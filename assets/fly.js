75179bc585f4bee9fbaa7f25b298f645dfe14c73
dylanocallaghan <dylan.oc.1999@gmail.com>
2021-04-24 11:27:34 +0000

    changed gametype and added fly.js and birdy.js

---
+++ assets/fly.js
@@ -0,0 +1,26 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+let spacePressed = false;
+let angle = 0;
+let hue = 0;
+let frame = 0;
+let score = 0;
+let gamespeed = 2;
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(10, 10, 50, 50);
+    requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener('keydown', function(e) {
+    if (e.code === 'Space') spacePressed = true;
+});
+window.addEventListener('keyup', function(e) {
+    if (e.code === 'Space') spacePressed = false;
+});
