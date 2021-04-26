5ccabade71346d4ae9563b8be02f7eaba5ddaea1
dylanocallaghan <dylan.oc.1999@gmail.com>
2021-04-26 19:39:06 +0000

    add hit detection and an image to pop up when the character hits a obstacle, also added the images to files so i can use it as a pop up

--- assets/fly.js
+++ assets/fly.js
@@ -15,9 +15,11 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //ctx.fillRect(10, 10, 50, 50);
     handleObstacles();
+    handleParticles();
     bird.update();
     bird.draw();
-    handleParticles();
+    handleCollisions();
+    if (handleCollisions()) return;
     requestAnimationFrame(animate);
     angle+=0.06;
     hue++;
@@ -31,3 +33,22 @@ window.addEventListener('keydown', function(e) {
 window.addEventListener('keyup', function(e) {
     if (e.code === 'Space') spacePressed = false;
 });
+
+const hit = new Image();
+hit.src = 'assets/hit.png';
+function handleCollisions() {
+    for (let i = 0; i < obstaclesArray.length; i++) {
+        if (bird.x < obstaclesArray[i].x + obstaclesArray[i].width && 
+            bird.x + bird.width > obstaclesArray[i].x &&
+            ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) ||
+            (bird.y > canvas.height - obstaclesArray[i].bottom &&
+            bird.y + bird.height < canvas.height))){
+                ctx.drawImage(hit, bird.x, bird.y, 50, 50); 
+                ctx.font = "25px sans-serif";
+                ctx.fillStyle = 'black';
+                ctx.fillText('Game Over, your score is ' + score, 160, canvas.height/2 - 10);
+                
+                return true;
+            }
+    }
+}
